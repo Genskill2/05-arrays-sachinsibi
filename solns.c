96850ae9cb277a4ba009961d982e2659aff67639
sachinsibi <84579547+sachinsibi@users.noreply.github.com>
2021-06-15 20:16:53 +0530

    Added average.c

--- solns.c
+++ solns.c
@@ -69,4 +69,30 @@ int min(int a[],int b) {
  return d;
 } 
 
+//average.c
+#include <stdio.h>
+#include <assert.h>
+
+
+float average(int [], int);
+
+int main(void) {
+ int x[] = {9,5,6,10,2,-3,4};
+ assert ((average(x, 7) - 4.7142) < 0.001);
+
+ int y[] = {5};
+ assert (average(y, 1) == 5.0);
+ printf("Average: passed\n");
+}
+
+float average(int a[], int b) {
+ int s = 0;
+ for (int i=0; i<b; i++) {
+  s = s+a[i];
+ }
+ float x = (float)s/b;
+ printf("Average is %f\n",x);
+ return x;
+}
+
 //next
