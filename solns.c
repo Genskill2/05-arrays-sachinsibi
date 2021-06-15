8b43eab2b88414fa13fa410caf85f907bc4d6aba
sachinsibi <84579547+sachinsibi@users.noreply.github.com>
2021-06-15 22:58:31 +0530

    Added mode.c

--- solns.c
+++ solns.c
@@ -95,4 +95,73 @@ float average(int a[], int b) {
  return x;
 }
 
+//mode.c
+#include <stdio.h>
+#include <assert.h>
+
+
+int mode(int [], int);
+
+int main(void) {
+ int x[] = {4,9,5,6,5,10,0,2,-3,-3,4,4};
+ assert ((mode(x, 12) == 4));
+
+ int y[] = {5};
+ assert (mode(y, 1) == 5);
+ printf("Mode: passed\n");
+}
+
+int mode(int a[], int b) {
+ int c1 = 0;
+ int c2 = 0;
+ int c3 = 0;
+ int m = 0;
+ if (b>1) {
+  for (int i=0; i<b-1; i++) {
+   for (int j=i+1; j<b; j++) {
+    if (a[i]==a[j]) {
+     c1 = c1 + 1;
+     /*printf("a[i] is %i and a[j] is %i\n",a[i],a[j]);
+     printf("C1 is %i\n",c1);
+     printf("C3 is %i\n",c3);*/
+    }
+    else {
+     /*printf("a[i] is %i and a[j] is %i\n",a[i],a[j]);
+     printf("%i\n",c1);
+     printf("C2 is %i\n",c2);
+     printf("C3 is %i\n",c3);*/
+    }
+   }
+  if (c1>c2) {
+   c2 = c1;
+   c1 = 0;
+   c3 = 0;
+   //printf("c2 is %i, c1 is %i and c3 is %i\n",c2,c1,c3);
+   m = a[i];
+   //printf("m is %i\n",m);
+  }
+  else if (c1==c2) {
+   c3 = c3 + 1;
+   c1 = 0;
+  }
+  else {
+   c1 = 0;
+   //printf("c2 is %i and c1 is %i\n",c2,c1);
+  }
+  }
+ }
+ else {
+  c2 = b-1;
+  m = a[0];
+ }
+ if (c3==0) {
+  printf("Mode is %i\nIt occurs %i time(s)\n",m,c2+1);
+ }
+ else {
+  printf("Multiple modes detected\n");
+  printf("First occurring mode is %i\nIt occurs %i time(s)\n",m,c2+1); 
+ }
+ return m;
+}
+
 //next
